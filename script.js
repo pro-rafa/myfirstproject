239fb87c30ba6bfe0fd391bf973c11c8cb36f229
rafael <raafhxs_spy@live.com>
2024-06-17 13:51:24 -0300

    slide do switch

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function toggleMode() (
-  
-)
+function toggleMode() {
+    const html = document.documentElement
+
+    //if(html.classList.contains('light')) {
+    //    html.classList.remove('light')
+    //} else {
+     //   html.classList.add('light')
+    // }
+
+    html.classList.toggle("light")
+}
